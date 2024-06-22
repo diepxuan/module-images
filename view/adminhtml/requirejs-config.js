15a5e3d9d5367b932e384dd1b1c8d2e87813decd
Tran Ngoc Duc <caothu91@gmail.com>
2024-06-22 15:32:26 +0700

    product media js allow upload webp

---
+++ view/adminhtml/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_Backend/js/media-uploader': {
+                'Diepxuan_Images/js/media-uploader-mixin': true
+            }
+        }
+    }
+};

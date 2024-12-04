20726649a1aef5b367deaf0e4523d5f59c594cfb
diepxuan <ductn@diepxuan.com>
2024-12-04 15:36:26 +0000

    cleanup code

--- Config/Model/Config/Backend/Image.php
+++ Config/Model/Config/Backend/Image.php
@@ -8,29 +8,22 @@ declare(strict_types=1);
  * @author     Tran Ngoc Duc <ductn@diepxuan.com>
  * @author     Tran Ngoc Duc <caothu91@gmail.com>
  *
- * @lastupdate 2024-12-04 17:34:09
+ * @lastupdate 2024-12-04 22:34:14
  */
 
 namespace Diepxuan\Images\Config\Model\Config\Backend;
 
 use Diepxuan\Images\Model\Extension;
-use Magento\Config\Model\Config\Backend\File\RequestData\RequestDataInterface;
 use Magento\Config\Model\Config\Backend\Image as OriginImage;
-use Magento\Framework\App\Cache\TypeListInterface;
-use Magento\Framework\App\Config\ScopeConfigInterface;
-use Magento\Framework\Data\Collection\AbstractDb;
-use Magento\Framework\Filesystem;
-use Magento\Framework\Model\Context;
-use Magento\Framework\Model\ResourceModel\AbstractResource;
-use Magento\Framework\Registry;
-use Magento\MediaStorage\Model\File\UploaderFactory;
 
 class Image extends OriginImage
 {
     private $extension;
 
-    protected function getExtension() : Extension {
-        $this->extension = $this->extension?:new Extension;
+    protected function getExtension(): Extension
+    {
+        $this->extension = $this->extension ?: new Extension();
+
         return $this->extension;
     }
 
